c9f864db6b94e089f6e640c02ab70d7af41323e6
putra.harianja <putra.harianja007@gmail.com>
2024-12-28 14:47:23 +0700

    enchance promt

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -5,7 +5,7 @@ const genAI = require('../config/googleAI')
 router.post('/generate', async (req, res) => {
     const { prompt } = req.body
 
-    const finalPrompt = `Write a short story for children, ${prompt}. Please return the story using HTML tags for headings and paragraphs, but exclude the <!DOCTYPE>, <html>, <head>, and <body> tags. The output should be formatted so that it can be directly inserted into a <span v-html="response"></span> element on the client side.`
+    const finalPrompt = `Write a short story for children, ${prompt}. Please return the story using HTML tags for headings and paragraphs, but exclude the <!DOCTYPE>, <html>, <head>, and <body> tags and also exclude this (\```html). The output should be formatted so that it can be directly inserted into a <span v-html="response"></span> element on the client side so the tag it used should be only <h1> and <p>`
 
     try {
         const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
